635d46680e89aa5b8a73a90c603f7eb3b9cb9b50
danjr26 <driehm1@nd.edu>
2018-04-07 13:27:28 -0400

    Betterified Go_To_End()

--- myTest.m
+++ myTest.m
@@ -3,17 +3,18 @@ nSecBuffer = 10;
 nSecDisplay = 5;
 
 stream = OpenBCIStream('OpenBCI-RAW-testing123.txt', sampleRate, 4, nSecBuffer);
+stream.Go_To_End();
 smoother = SettleSmoother([4, sampleRate * nSecDisplay]);
 hold off
 figure(1);
 clf
 while true
-	nSteps = stream.Read_New_Data(0.08) * sampleRate;
+	timeRead = stream.Read_New_Data(0.08) * sampleRate;
 	[x, y] = stream.Get_Raw_Data(-nSecDisplay, 0);
 
 	[freqX, freqY] = To_Frequencies(y, nSecDisplay);
 	y2 = Isolate_Frequency_Range(y, freqX, freqY, 18, 25);
-	smoother.Feed_Data(y2, nSteps);
+	smoother.Feed_Data(y2, timeRead);
 	y3 = smoother.Get_Data();
 	
 	subplot(4, 1, 1);
