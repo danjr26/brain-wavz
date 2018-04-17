4cdee1e1f15f446c2d0d43e2e487071c2c66651b
danjr26 <driehm1@nd.edu>
2018-04-17 00:32:27 -0400

    Commented and cleaned up some

--- Isolate_Frequency_Range.m
+++ Isolate_Frequency_Range.m
@@ -1,8 +1,30 @@
 function data = Isolate_Frequency_Range...
 	(rawData, freqX, freqY, lowFreq, highFreq)
 
+% Created by Daniel Riehm
+% Last edited 16 April 2018
+
+% Given an array of raw data and pre-calculated frequencies, assembles
+% copy of input data composed only of frequencies in selected range
+
+% Input Variables
+% rawData :		m by n array of evenly-spaced y-values of m different
+%					unprocessed functions 
+% freqX :		m by n array of frequency x-values (Hz)
+% freqY :		m by n array of frequency y-values (amplitude)
+% lowFreq :		value representing lower end of desired frequency range
+% highFreq :	value representing higher end of desired frequency rnage
+
+% Output Variables
+% data :	m by n array of evenly-spaced y-values of m different processed
+%				functions
+
+%% Initialize variables
+
 dataSize = size(rawData);
 newFreqY = zeros(dataSize);
+
+%% Set unwanted frequencies to 0
 
 for iRow = 1:dataSize(1)
 	for iColumn = 1:dataSize(2)
@@ -13,5 +35,7 @@ for iRow = 1:dataSize(1)
 		end
 	end
 end
+
+%% Reassemble from altered frequencies
 
 [~, data] = From_Frequencies(newFreqY, 0, 1);
--- OpenBCIStream.m
+++ OpenBCIStream.m
@@ -1,14 +1,34 @@
+% Created by Daniel Riehm
+% Last edited 16 April 2018
+
+% It's a class and not a bunch of separate functions because it's more
+% elegant this way 
+
+% Continuously opens, reads, and closes file to which the OpenBCI software is
+% writing the data, live.
+
+% I wasn't sure how to comment this in the "cell" format so I just did the
+% best I could...
+
 classdef OpenBCIStream < handle
 	properties (SetAccess = private)
+		% where we left off in the file
 		charOffset
+		
 		fileName
+		
+		% Hz
 		sampleRate
+		
+		% [nRows, nColumns]
 		bufferSize
+		
+		% stays constant size, sheds old data
 		buffer
-		bufferOffset
 	end
 	
 	methods
+		% Constructor
 		function this = OpenBCIStream(fileName_in, sampleRate_in, ...
 				nChannels_in, bufferLength_in)
 			this.charOffset = 0;
@@ -16,9 +36,13 @@ classdef OpenBCIStream < handle
 			this.sampleRate = sampleRate_in;
 			this.bufferSize = [nChannels_in, bufferLength_in * sampleRate_in];
 			this.buffer = zeros(this.bufferSize);
-			this.bufferOffset = 0;
 		end
 		
+		% Reads data in file from where it left off, until it hits the end
+		% of the file or reads <maxTime> seconds, whichever comes
+		% first. <maxTime> can be used to throttle the speed when reading
+		% from recorded (non-live) data, but should be set to a high number
+		% when live-reading. 
 		function timeRead = Read_New_Data(this, maxTime)
 			% setup file reading
 			fileID = fopen(this.fileName, 'r');
@@ -76,8 +100,12 @@ classdef OpenBCIStream < handle
 				%% the rest of the values can be ignored
 			end
 			
+			% remember where we left off
 			this.charOffset = ftell(fileID);
 			
+			fclose(fileID);
+			
+			% incorporate new data into buffer
 			if nLines == this.bufferSize(2)
 				this.buffer = newData;
 			else
@@ -86,11 +114,15 @@ classdef OpenBCIStream < handle
 			end
 			
 			timeRead = nLines / this.sampleRate;
-			
-			fclose(fileID);
 		end
 		
+		% Gets data from all channels from time1 to time2, both of which
+		% should be nonpositive (since a time of -5 means 5 seconds ago, 
+		% positive times would mean requesting data that hasn't been read
+		% yet, and while I'm good, I'm not quite that good)
 		function [x, y] = Get_Raw_Data(this, time1, time2)
+			% yay parameter screening because it's not necessarily
+			% intuitive
 			if time1 > 0 || time1 < -this.bufferSize(2) / this.sampleRate
 				error('time1 is out of range');
 			end
@@ -101,9 +133,11 @@ classdef OpenBCIStream < handle
 				error('time1 > time2');
 			end
 			
+			% turns time indices into buffer indices
 			index2 = floor(time1 * -this.sampleRate);
 			index1 = floor(time2 * -this.sampleRate) + 1;
 			
+			% returns 
 			x = linspace(time2, time1, index2 - index1 + 1);
 			y = this.buffer(:, index1:index2);
 		end
@@ -112,6 +146,8 @@ classdef OpenBCIStream < handle
 			this.charOffset = 0;
 		end
 		
+		% Do this  to catch up with the live data, skipping everything in
+		% between 
 		function Go_To_End(this)
 			fileID = fopen(this.fileName, 'r');
 			fseek(fileID, this.charOffset, 'bof');
