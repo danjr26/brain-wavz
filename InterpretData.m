c8a8dc7b756249e2f577be19ac4766288b371413
Robert Schaefer <rschaef1@nd.edu>
2018-04-23 21:06:40 -0400

    InterpretChanges

--- InterpretData.m
+++ InterpretData.m
@@ -1,29 +1,31 @@
-function [dot, dash] = InterpretData(y3)
+function [y3, y4 , code] = InterpretData(num, y3)
 
 % Robbie Schaefer
 % Brain WAVS project
 
 %% interpret received data
 
-threshold = -1150;      % threshold value for when the computer reads a 'tick'
+threshold = -25;      % threshold value for when the computer reads a 'tick'
 dot = false;        % This will require tons of calibration
 dash = false;       % Future plans: Make it more specific for each channel
+space = false;
 data = y3;
 [~,N] = size(y3);
-usable = N -10;
-code = 0;
+
+usable = num + N-33;
+code = '0';
 counter = 0;
 
 for i = 1:usable
     if data(1,i) > threshold
         if data (2,i) > threshold
-            for k = 1:10
+            for k = 1:33
                 l = k + i;
                 if data(1, l) > threshold && data(2,l) > threshold
                     counter = counter + 1;
                 end
             end
-            if counter >=8
+            if counter >= 31
                 space = true;
             end
         else
@@ -48,14 +50,18 @@ for i = 1:usable
         end
     end
     if dot == true
-        code = [code + '-.'];
+        code = [code, '10'];
+        i = i + 20;
     elseif dash == true
-        code = [code + '---.'];
+        code = [code, '1110'];
+        i = i + 20;
     elseif space == true
-        code = [code + '...']
+        code = [code, '000'];
+        i = i + 53'
     end
     counter = 0;
     space = false;
     dash = false;
     dot = false;
+    num = num + 1;
 end
--- InterpretDataCopy.m
+++ InterpretDataCopy.m
@@ -1,29 +1,52 @@
-function [dot, dash] = InterpretData(y3)
+function [newdata, y4 , code] = InterpretData(num, olddata, newdata)
 
 % Robbie Schaefer
 % Brain WAVS project
 
-%% interpret received data
+%% Set Parameters
 
-threshold = -1150;      % threshold value for when the computer reads a 'tick'
+threshold = -25;      % threshold value for when the computer reads a 'tick'
 dot = false;        % This will require tons of calibration
 dash = false;       % Future plans: Make it more specific for each channel
-data = y3;
-[~,N] = size(y3);
-usable = N -10;
-code = 0;
+space = false;
+data = newdata;
+[~,N] = size(newdata);
+
+%% Find out how much of the data is new
+y3 = newdata;
+caughtup = false;
+n = 1;
+length3 = size(olddata);
+oldlength = length3(1,1);
+length4 = size(newdata);
+newlength = length4(1,1);
+
+    while caughtup == false
+        if olddata(1, oldlength) == newdata(1, n)
+            caughtup = true;
+            newdata = newdata(:, [n, newlength]);
+        else
+            n = n+1;
+        end
+    end
+    
+%% Interpret the data
+
+data = newdata;
+usable =  N-33;
+code = '0';
 counter = 0;
 
 for i = 1:usable
     if data(1,i) > threshold
         if data (2,i) > threshold
-            for k = 1:10
+            for k = 1:33
                 l = k + i;
                 if data(1, l) > threshold && data(2,l) > threshold
                     counter = counter + 1;
                 end
             end
-            if counter >=8
+            if counter >= 31
                 space = true;
             end
         else
@@ -48,11 +71,14 @@ for i = 1:usable
         end
     end
     if dot == true
-        code = [code + '-.'];
+        code = [code, '10'];
+        i = i + 20;
     elseif dash == true
-        code = [code + '---.'];
+        code = [code, '1110'];
+        i = i + 20;
     elseif space == true
-        code = [code + '...']
+        code = [code, '000'];
+        i = i + 53;
     end
     counter = 0;
     space = false;
